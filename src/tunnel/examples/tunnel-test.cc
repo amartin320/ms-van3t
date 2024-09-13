ff7369b92ef8e634da3ab3274813aff4dbd461a2
amartin320 <aitor.mrod@gmail.com>
2024-09-13 15:41:19 +0200

    Added a test application for new class development

---
+++ src/tunnel/examples/tunnel-test.cc
@@ -0,0 +1,25 @@
+#include "ns3/tunnel-module.h"
+#include "ns3/string.h"
+#include "ns3/yans-wifi-helper.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("tunnel-test");
+
+int main (int argc, char *argv[])
+{
+    LogComponentEnable ("obu", LOG_LEVEL_ALL);
+
+    // Initialize propagation channel
+    YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+    Ptr<YansWifiChannel> channel = wifiChannel.Create ();
+    wifiChannel.AddPropagationLoss("ns3::FriisPropagationLossModel");
+
+    // Initialize OBU
+
+    Ptr<obu> obu1 = CreateObject<obu> ("802.11p");
+    obu1->SetChannel(channel);
+    obu1->initialize();
+    
+    return 0;
+}
