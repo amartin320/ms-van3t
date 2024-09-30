8a49cf1ce2a260540ff664b4c718ed107873275b
amartin320 <aitor.mrod@gmail.com>
2024-09-30 11:17:17 +0200

    Enable SUDO when building

--- ns3_build.sh
+++ ns3_build.sh
@@ -1,2 +1,2 @@
-ns-3-dev/ns3 configure --build-profile=optimized --enable-examples --enable-tests --disable-python --disable-werror --enable-logs
+ns-3-dev/ns3 configure --build-profile=optimized --enable-examples --enable-tests --disable-python --disable-werror --enable-logs --enable-sudo
 ns-3-dev/ns3 build
